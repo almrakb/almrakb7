b46a2ec2e08347d5388305bc22d71d0f75d7e78a
almrakb <almrakb2024@gmail.com>
2025-08-06 18:25:31 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,45 @@
+import './globals.css';
+import Script from 'next/script';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Almrakb Landing Page',
+  description: 'شركة المراكب للشحن والخدمات اللوجستية',
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar">
       <head>
         {/* Google Analytics */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-78MQL5N3P"></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-78MQL5N3P" />
+        <Script id="google-analytics">
+          {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-78MQL5N3P');
-          `,
-        }} />
+          `}
+        </Script>
 
         {/* TikTok Pixel */}
-        <script dangerouslySetInnerHTML={{
-          __html: `
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
             !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
-              ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-              ttq.load=function(e){var n=d.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid="+e+"&lib=ttq";
-              var a=d.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a)};
-              ttq.load('D29MT83C7TUCUVSB65KG'); ttq.page();
+              w.TiktokAnalyticsObject = t; var ttq = w[t] = w[t] || [];
+              ttq.methods = ["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+              ttq.setAndDefer = function (t, e) { t[e] = function () { t.push([e].concat(Array.prototype.slice.call(arguments, 0))) } };
+              for (var i = 0; i < ttq.methods.length; i++) ttq.setAndDefer(ttq, ttq.methods[i]);
+              ttq.load = function (e, n) { var i = "https://analytics.tiktok.com/i18n/pixel/events.js"; ttq._i = ttq._i || {}; ttq._i[e] = []; ttq._t = ttq._t || {}; ttq._t[e] = +new Date; ttq._o = ttq._o || {}; ttq._o[e] = n || {}; var a = document.createElement("script"); a.type = "text/javascript"; a.async = !0; a.src = i + "?sdkid=" + e + "&lib=" + t; var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(a, s) };
+              ttq.load('D29MT83C7TUCUVSB65KG');
+              ttq.page();
             }(window, document, 'ttq');
-          `,
-        }} />
+          `}
+        </Script>
       </head>
-      <body>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
