a70c3ce085943c82b3e161dacd5b00238d6a966a
almrakb <almrakb2024@gmail.com>
2025-08-06 18:18:50 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,60 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Script from 'next/script'
-
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'Almrakb Landing Page',
-  description: 'شركة المراكب للشحن والخدمات اللوجستية',
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ar">
       <head>
-        {/* ✅ Google Analytics */}
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-78MQL5N3P" strategy="afterInteractive" />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-78MQL5N3P"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-78MQL5N3P');
-          `}
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-78MQL5N3P" strategy="afterInteractive" />
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-78MQL5N3P');
-  `}
-</Script>
+          `,
+        }} />
 
-
-        {/* ✅ TikTok Pixel */}
-        <Script id="tiktok-pixel" strategy="afterInteractive">
-          {`
+        {/* TikTok Pixel */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
             !function (w, d, t) {
-              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
-              ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"];
               ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-              for(var i=0;i<ttq.methods.length;i++){ttq.setAndDefer(ttq,ttq.methods[i])}
-              ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";
-              ttq._i=ttq._i||{};ttq._i[e]=[];ttq._i[e]._u=i;ttq._t=ttq._t||{};ttq._t[e]=+new Date;
-              ttq._o=ttq._o||{};ttq._o[e]=n||{};var o=document.createElement("script");
-              o.type="text/javascript";o.async=!0;o.src=i;var a=document.getElementsByTagName("script")[0];
-              a.parentNode.insertBefore(o,a)};
-              ttq.load('XXXXXXXXXX'); // ضع هنا TikTok Pixel ID
-              ttq.page();
+              for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
+              ttq.load=function(e){var n=d.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://analytics.tiktok.com/i18n/pixel/events.js?sdkid="+e+"&lib=ttq";
+              var a=d.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a)};
+              ttq.load('D29MT83C7TUCUVSB65KG'); ttq.page();
             }(window, document, 'ttq');
-          `}
-        </Script>
+          `,
+        }} />
       </head>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
